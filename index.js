305e6343373f3019f7c5297370ff568e8fc35681
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-08 18:04:07 +0600

    add jwt

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // samratahammed29
 // uK2a2DjvyJHj7JJE
 
-const uri =
-  "mongodb+srv://samratahammed29:uK2a2DjvyJHj7JJE@cluster0.kbligoj.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbligoj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,10 +27,46 @@ const client = new MongoClient(uri, {
   },
 });
 
+const logger = async (req, res, next) => {
+  console.log("called", req.host, req.originalUrl);
+  next();
+};
+
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token;
+//   console.log("value of token of middleware", token);
+//   if (!token) {
+//     return res.status(401).send({ message: "forbidden" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized" });
+//     }
+//     console.log("value in token ", decode);
+//     req.user = decode;
+//     next();
+//   });
+// };
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     const assignmentCollection = client
       .db("assignmentDB")
@@ -83,25 +123,12 @@ async function run() {
       res.send(result);
     });
 
-    // app.patch("/allAssignment/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateChecking = req.body;
-    //   // console.log(updateChecking);
-    //   const updateDoc = {
-    //     $set: {
-    //       status: updateChecking.status,
-    //     },
-    //   };
-
-    //   const result = await assignmentCollection.updateOne(filter, updateDoc);
-    //   console.log(result);
-    //   res.send(result);
-    // });
-
     app.get("/takeAssignment", async (req, res) => {
       const { email } = req.query;
-
+      console.log("token:", req.cookies.token);
+      // if (req.query.email !== req.user.email) {
+      //   return res.status(403).send({ message: "forbidden access" });
+      // }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
